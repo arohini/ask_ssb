d2d53c38603346c557ff1056c06fd72fc4611d0a
rohini <rohini@gmail.com>
2025-10-17 15:42:04 +0530

    Add core value template file

---
+++ ssb_values.py
@@ -0,0 +1,9 @@
+ssb_core_values = ["Faith", "Shraddha", "Devotion", "Patience", "Saburi","Compassion"
+                   "Charity", "Equality", "Truthfulness", "Simplicity", "Forgiveness"]
+
+ssb_practices = ["Nama", "Bhagavad Gita", "Guru Charitra", "Ramayana", "Sai Satcharitra", "Seva"
+"Meditation", "Prayer"]
+
+ssb_things_accepted = ["Food", "Lamps", "Udi", "Helping", "Feeding", "Poor", "Annadanam"]
+
+ssb_sayings = ["Allah Malik", "Why fear when I am here"]
